507c5ca5bfa87a64835e0d362838d01f8529a382
endeavorLiu <904915515@qq.com>
2021-12-11 17:29:07 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: '不远遐路，幸见光临',
+  head:[
+    ['link',{rel:'icon',href:'/favicon.ico'}]
+  ],
+  description: 'Just playing around',
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
