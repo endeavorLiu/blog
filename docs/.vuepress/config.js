2ce98bfda93173074461cb3c04dd6019b4aaca38
endeavorLiu <904915515@qq.com>
2021-12-11 18:32:43 +0800

    pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,27 @@
 module.exports = {
-  base:'/blog/',
+  base: '/blog/',
   title: '不远遐路，幸见光临',
-  head:[
-    ['link',{rel:'icon',href:'/favicon.ico'}]
-  ],
   description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  plugins: [
+    '@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: {
+        message: "发现新内容可用",
+        buttonText: "刷新"
+      }
+    }
+  ],
   themeConfig: {
     logo: '/assets/img/logo.png',
     nav: [
