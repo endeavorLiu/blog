f6fe3b71a0bf56beb82732dbd7b2698932623cfb
endeavorLiu <904915515@qq.com>
2021-12-11 21:46:02 +0800

    隐藏私密信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,48 +1,12 @@
+const headConf = require("./config/headConf")
+const pluginsConf = require("./config/pluginsConf")
+const themeConf = require("./config/themeConf")
+
 module.exports = {
   base: '/blog/',
   title: '不远遐路，幸见光临',
   description: 'Just playing around',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
-  ],
-  plugins: {
-    '@vuepress/pwa': {
-      serviceWorker: true,
-      updatePopup: {
-        message: "发现新内容可用",
-        buttonText: "刷新"
-      }
-    },
-    '@vssue/vuepress-plugin-vssue': {
-      // 设置 `platform` 而不是 `api`
-      platform: 'github-v4',
-
-      // 其他的 Vssue 配置
-      owner: 'endeavorLiu',
-      repo: 'blog',
-      clientId: '7261cae94d500fb41b64',
-      clientSecret: 'f60cbb49b75d71c6d62f397048b6dc9c2f00df50',
-      autoCreateIssue: true
-    },
-    '@vuepress/back-to-top': true,
-    '@vuepress/google-analytics': {
-      'ga': '296511250'
-    }
-  },
-  themeConfig: {
-    logo: '/assets/img/logo.png',
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'About', link: '/about/' },
-      { text: 'External', link: 'https://google.com' },
-    ]
-  }
+  head: headConf,
+  plugins: pluginsConf,
+  themeConfig: themeConf
 }
---
+++ docs/.vuepress/config/themeConf.js
@@ -0,0 +1,8 @@
+module.exports = {
+  logo: '/assets/img/logo.png',
+  nav: [
+    { text: 'Home', link: '/' },
+    { text: 'About', link: '/about/' },
+    { text: 'External', link: 'https://google.com' },
+  ]
+}
