5b8d6099be1c015b9f47f4657cac72b8292bb809
endeavorLiu <904915515@qq.com>
2021-12-13 01:29:37 +0800

    update

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,22 +1,22 @@
 import iconfont from './public/iconfont/iconfont.css'
-import nprogress from 'nprogress'
+// import nprogress from 'nprogress'
 
-export default ({ router }) => {
-  router.beforeEach((to, from, next) => {
-    nprogress.start()
-    next()
-  });
-  router.afterEach((to, from) => {
-    nprogress.done()
-  });
-  window.addEventListener('scroll', handleScroll);
-  function handleScroll() {
-    // 屏幕剩余的高度
-    let surplus = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    // let screenH = document.documentElement.clientHeight
-    // 当前滑动的高度
-    let scrollY = document.documentElement.scrollTop
-    let coorY = scrollY / surplus;
-    nprogress.set(coorY)
-  }
-}
+// export default ({ router }) => {
+//   router.beforeEach((to, from, next) => {
+//     nprogress.start()
+//     next()
+//   });
+//   router.afterEach((to, from) => {
+//     nprogress.done()
+//   });
+//   window.addEventListener('scroll', handleScroll);
+//   function handleScroll() {
+//     // 屏幕剩余的高度
+//     let surplus = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//     // let screenH = document.documentElement.clientHeight
+//     // 当前滑动的高度
+//     let scrollY = document.documentElement.scrollTop
+//     let coorY = scrollY / surplus;
+//     nprogress.set(coorY)
+//   }
+// }
